fd94464e0cc676adb7387048eda755c4bb89f5d2
Anatoliy Yakimov <yakimov.tolya@gmail.com>
2019-07-22 22:58:46 +0300

    Add base CRUD methods for RoomService

--- src/app/rooms/rooms.service.ts
+++ src/app/rooms/rooms.service.ts
@@ -1,10 +1,42 @@
-import {Injectable} from '@angular/core';
+import {Injectable, Provider} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {Room} from '../_shared/entities/room/room';
+import {environment} from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RoomsService {
 
-  constructor() {
+  private API = environment.API_URL;
+
+  constructor(private http: HttpClient) {
   }
+
+  getAllRooms(): Observable<Room[]> {
+    return this.http.get<Room[]>(this.API + 'rooms/');
+  }
+
+  getRoomById(id: number): Observable<Room> {
+    return this.http.get<Room>(`${this.API}/rooms/${id}`);
+  }
+
+  deleteRoom(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.API}/rooms/${id}`);
+  }
+
+  updateRoom(id: number, room: Room): Observable<Room> {
+    return this.http.put<Room>(`${this.API}/rooms/${id}`, room);
+  }
+
+  saveRoom(room: Room): Observable<Room> {
+    return this.http.post<Room>(`${this.API}/rooms`, room);
+  }
+
 }
+
+export const ROOM_SERVICE_PROVIDER: Provider = {
+  provide: RoomsService,
+  useClass: RoomsService
+};
