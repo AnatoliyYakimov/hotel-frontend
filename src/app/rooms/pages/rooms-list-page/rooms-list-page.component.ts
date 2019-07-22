0cfd91283246fc349c198f73a301800a075d6513
Anatoliy Yakimov <yakimov.tolya@gmail.com>
2019-07-22 22:19:33 +0300

    Add empty components

---
+++ src/app/rooms/pages/rooms-list-page/rooms-list-page.component.ts
@@ -0,0 +1,16 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-rooms-list-page',
+  templateUrl: './rooms-list-page.component.html',
+  styleUrls: ['./rooms-list-page.component.scss']
+})
+export class RoomsListPageComponent implements OnInit {
+
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/rooms/room-details-page/room-details-page.component.ts
@@ -0,0 +1,16 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-room-details-page',
+  templateUrl: './room-details-page.component.html',
+  styleUrls: ['./room-details-page.component.scss']
+})
+export class RoomDetailsPageComponent implements OnInit {
+
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
+
+}
