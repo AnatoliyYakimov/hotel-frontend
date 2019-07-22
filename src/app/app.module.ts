4642a5767a5db2ef6c6529e92eaf57f7d6ef9839
Anatoliy Yakimov <yakimov.tolya@gmail.com>
2019-07-22 22:15:44 +0300

    Fix impotrs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,13 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import {
-  MatButtonModule,
-  MatFormFieldModule,
-  MatIconModule,
-  MatInputModule,
-  MatMenuModule, MatRadioModule,
-  MatSelectModule
-} from '@angular/material';
-import {MatToolbarModule} from '@angular/material';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {MatButtonModule, MatToolbarModule} from '@angular/material';
 import {HeaderComponent} from './header/header.component';
 import {BodyComponent} from './body/body.component';
 import {FooterComponent} from './footer/footer.component';
-import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {RoomsComponent} from './rooms/rooms.component';
 import {RoomsListComponent} from './rooms/rooms-list/rooms-list.component';
 import {RoomDetailsComponent} from './rooms/room-details/room-details.component';
