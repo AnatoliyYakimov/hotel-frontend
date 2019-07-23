00353cecb8188709b12b73f0d41305edfc33e85e
Anatoliy Yakimov <yakimov.tolya@gmail.com>
2019-07-24 00:23:02 +0300

    Remove console logger interceptor

--- src/app/_interceptors/interceptors-provider.ts
+++ src/app/_interceptors/interceptors-provider.ts
@@ -1,17 +1,12 @@
 import {Provider} from '@angular/core';
 import {HTTP_INTERCEPTORS} from '@angular/common/http';
 import {ContentTypeJsonInterceptor} from './content-type-json-interceptor';
-import {SimpleLogInterceptor} from './simple-log-interceptor';
 
-export const InterceptorsProvider: Provider[] = [
+
+export const INTERCEPTOR_PROVIDER: Provider[] = [
   {
     provide: HTTP_INTERCEPTORS,
     useClass: ContentTypeJsonInterceptor,
-    multi: true
-  },
-  {
-    provide: HTTP_INTERCEPTORS,
-    useClass: SimpleLogInterceptor,
     multi: true
   }
 ];
