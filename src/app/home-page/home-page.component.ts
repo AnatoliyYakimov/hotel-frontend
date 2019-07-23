9993107d06f87a789be5af2a5831be5daca39ebf
Anatoliy Yakimov <yakimov.tolya@gmail.com>
2019-07-24 00:55:01 +0300

    Add routing with params

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,6 +1,8 @@
 import {Component, OnInit} from '@angular/core';
 import * as moment from 'moment';
 import {Moment} from 'moment';
+import {Router} from '@angular/router';
+import {DATE_CONSTANTS} from '../_shared/date-constants';
 
 @Component({
   selector: 'app-home-page',
@@ -11,7 +13,16 @@ export class HomePageComponent implements OnInit {
   checkIn: Moment = moment(Date.now());
   checkOut: Moment = moment(this.checkIn).add(1, 'days');
 
-  constructor() {
+  constructor(private router: Router) {
+  }
+
+  onClick() {
+    this.router.navigate(['rooms'], {
+      queryParams: {
+        checkIn: this.checkIn.format(DATE_CONSTANTS.SHORTDATE),
+        checkOut: this.checkOut.format(DATE_CONSTANTS.SHORTDATE)
+      }
+    });
   }
 
   ngOnInit() {
